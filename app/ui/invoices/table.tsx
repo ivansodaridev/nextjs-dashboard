d1ab6eea28198d217114bbd4ea58f69cd98f969b
Ivan <ivan.sodari@nerito.com>
2023-11-15 12:51:16 -0700

    Chapter 16

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -28,7 +28,7 @@ export default async function InvoicesTable({
                     <div className="mb-2 flex items-center">
                       <Image
                         src={invoice.image_url}
-                        alt={`${invoice.image_url}`}
+                        alt={`${invoice.name}'s profile picture`}
                         className="mr-2 rounded-full"
                         width={28}
                         height={28}
@@ -87,7 +87,7 @@ export default async function InvoicesTable({
                     <div className="flex items-center gap-3">
                       <Image
                         src={invoice.image_url}
-                        alt={`${invoice.image_url}`}
+                        alt={`${invoice.name}'s profile picture`}
                         className="rounded-full"
                         width={28}
                         height={28}
